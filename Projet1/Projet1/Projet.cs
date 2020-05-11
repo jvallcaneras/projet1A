32d945d010b12f5e6363fefbacc0f760e469fbf6
Anthony Nguyen <anctrang@ensc.fr>
2020-05-11 21:30:51 +0200

    ajout constructeur

--- Projet1/Projet1/Projet.cs
+++ Projet1/Projet1/Projet.cs
@@ -7,66 +7,44 @@ namespace Projet1
     class Projet
     {
         //Attributs de la classe
-        private String _nomProjet;
+        private string _nomProjet;
         private int _typeProjet; // 1 - Transdi / 2 - transpromo / 3 - PFE / 4 - Intro prog / 5 - autre (à étoffer)
         private int _dureeSemaine;
         private int _nbIntervenant;
         private int _nbRole;  //A VOIR
         private int _nbLivrable;
         private int _nbMatiere;
+        private List<string> _matieres;
+        private List<string> _livrables;
+
+       
 
         //Constructeurs
+        public Projet(string nomProjet, int typeProjet, int dureeSemaine, int nbIntervenant, int nbLivrables, int nbMatiere, List<string> livrables, List<string> matières)
+        {
+            this.NomProjet = nomProjet;
+            this.TypeProjet = typeProjet;
+            this.DureeSemaine = dureeSemaine;
+            this.NbIntervenant = nbIntervenant;
+            this.NbLivrable = nbLivrables;
+            this.NbMatiere = nbMatiere;
+            this.Livrables = livrables;
+            this.Matieres = Matieres;
+        }
 
         //Propritétés
-        public String get_nomProjet()
-        {
-            return _nomProjet;
-        }
-        public int get_dureeSemaine()
-        {
-            return _dureeSemaine;
-        }
-        public int get_nbIntervenant()
-        {
-            return _nbIntervenant;
-        }
-        public int get_nbRole()
-        {
-            return _nbRole;
-        }
-        public int get_nbLivrable()
-        {
-            return _nbLivrable;
-        }
-        public int get_nbMatiere()
-        {
-            return _nbMatiere;
-        }
 
-        public void setnomProjet(string newNomProjet)
-        {
-            _nomProjet = newNomProjet;
-        }
-        public void setdureeSemaine(int newDureeSemaine)
-        {
-            _dureeSemaine = newDureeSemaine;
-        }
-        public void setnbIntervenant(int newNbIntervenant)
-        {
-            _nbIntervenant = newNbIntervenant;
-        }
-        public void setnbRole(int newNbRole)
-        {
-            _nbRole = newNbRole;
-        }
-        public void setnbLivrable(int newNbLivrable)
-        {
-            _nbLivrable = newNbLivrable;
-        }
-        public void setnbMatiere(int newNbMatiere)
-        {
-            _nbMatiere = newNbMatiere;
-        }
+        public int TypeProjet { get => _typeProjet; set => _typeProjet = value; }
+        public string NomProjet { get => _nomProjet; set => _nomProjet = value; }
+        public int DureeSemaine { get => _dureeSemaine; set => _dureeSemaine = value; }
+        public int NbIntervenant { get => _nbIntervenant; set => _nbIntervenant = value; }
+        public int NbRole { get => _nbRole; set => _nbRole = value; }
+        public int NbLivrable { get => _nbLivrable; set => _nbLivrable = value; }
+        public int NbMatiere { get => _nbMatiere; set => _nbMatiere = value; }
+        public List<string> Matieres { get => _matieres; set => _matieres = value; }
+        public List<string> Livrables { get => _livrables; set => _livrables = value; }
+
+
 
         //Methodes
 
