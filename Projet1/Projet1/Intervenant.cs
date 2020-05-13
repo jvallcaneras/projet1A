a38d21e08de71b2b2c2a2f6920fd2052dd49086d
Julie Vallcaneras <jvallcaneras@ensc.fr>
2020-05-13 19:05:27 +0200

    maj

--- Projet1/Projet1/Intervenant.cs
+++ Projet1/Projet1/Intervenant.cs
@@ -1,16 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Projet1
 {
-    class Intervenant             
+    class Intervenant
     {
         //Attributs de la classe
-        private String _nom;
-        private String _prenom;
-        private String _type; //1-etudiant, 2-prof, 3-intervnenant exte
+        private string _nom;
+        private string _prenom;
+        private string _type;
+        private int _reference; //1-etudiant, 2-prof, 3-intervnenant exte
 
+        //
+        public string Nom { get => _nom; set => _nom = value; }
+        public string Prenom { get => _prenom; set => _prenom = value; }
+        public string Type { get => _type; set => _type = value; }
+        public int Reference { get => _reference; set => _reference = value; }
 
+        //Constructeur
+        public Intervenant(string nom, string prenom, string type, int reference)
+        {
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.Type = type;
+            this.Reference = reference;
+        }
+
+        //Methodes
+        public static void CreationIntervenant(int nbIntervenant)
+        {
+
+            Console.Clear();
+            Console.WriteLine("\n\t \t \t \t\t \t \t \t Gestionnaire de projets de l'Ecole Nationale Supérieure de Cognitique");
+            Console.WriteLine("\n \n \n \n \n \n \n \n \n \n");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Intervenant>)); // Initialisation de l'outils de serialisation
+            List<Intervenant> dezerializedList = null; // Pour que la liste soit accessible en dehors du using filestream...
+            using (FileStream stream = File.OpenRead("intervenants.xml"))
+            {
+                dezerializedList = (List<Intervenant>)serializer.Deserialize(stream); // On récupère le contenu du fichier que l'on met dans notre liste
+            }
+            foreach (Intervenant i in dezerializedList)// Pour chaque matière récupérée dans la liste desérialisée
+            {
+                Console.WriteLine(i.Reference + "pour" + i.Type); // On affiche les attributs de la matière
+            }
+
+            int cpt = 0;
+            List<Intervenant> listeIntervenant = new List<Intervenant>();
+
+            while (cpt < nbIntervenant)
+            {
+                Console.WriteLine("Indiquez le nom de l'intervenant");
+                string nom = Console.ReadLine();
+                Console.WriteLine("Indiquez le prenom de l'intervenant");
+                string prenom = Console.ReadLine();
+                Console.WriteLine("Indiquez la catégorie de l'intervenant");
+                cpt++;
+                int entreeUtilisateur = Convert.ToInt32(Console.ReadLine());
+                switch (entreeUtilisateur)
+                {
+                    case 1:
+                        //Appeler fonction création étu;
+                        break;
+                    case 2:
+                        //Appeler fonction création prof;
+                        break;
+                    case 3:
+                        //Appeler fonction création exte;
+                        break;
+                }
+
+            }
+        }
     }
 }
--- Projet1/Projet1/Livrable.cs
+++ Projet1/Projet1/Livrable.cs
@@ -11,10 +11,12 @@ namespace Projet1
     {
         //Attributs de la classe
         private int reference;
+        private string _typeLivrable;
         private string _dateRendu;
 
         public string dateRendu { get => _dateRendu; set => _dateRendu = value; }
         public int Ref { get => reference; set => reference = value; }
+        public string typeLivrable { get => _typeLivrable; set => _typeLivrable = value; }
 
         //Constructeur
         public Livrable(string date, int reference)
@@ -39,9 +41,9 @@ namespace Projet1
             }
             foreach (Livrable l in dezerializedList)// Pour chaque matière récupérée dans la liste desérialisée
             {
-                Console.WriteLine(l.Ref + "pour" + l.TypeLivrable); // On affiche les attributs de la matière
+                Console.WriteLine(l.Ref + "pour" + l.typeLivrable); // On affiche les attributs de la matière
             }
-            Console.WriteLine("Indiquez les livrables attendus ? (Choisissez le chiffre associé au type de livrable. Tapez 0 si le livrable n'est pas dans la liste");
+            Console.WriteLine("Indiquez les livrables attendus (Choisissez le chiffre associé au type de livrable. Tapez 0 si le livrable n'est pas dans la liste");
             int cpt = 0;
             List<Livrable> listeLivrables = new List<Livrable>();
             while (cpt < nbLivrable)
@@ -53,6 +55,8 @@ namespace Projet1
                     Console.WriteLine("Veuillez saisir le nom de la matière.");
                     Livrable liv = new Livrable(Console.ReadLine(), dezerializedList.Count + 1); // On créé le nouveau livrable
                     dezerializedList.Add(liv);
+                    Console.WriteLine("Veuillez saisir la date de rendu.");
+                    liv.dateRendu = Console.ReadLine();
                     listeLivrables.Add(liv);
                     XmlSerializer xs1 = new XmlSerializer(typeof(List<Livrable>));
                     using (StreamWriter wr = new StreamWriter("livrables.xml"))
@@ -63,18 +67,14 @@ namespace Projet1
                 else
                 {
                     foreach (Livrable l in dezerializedList)
-                    {
+                    { 
                         if (entreeUtilisateur == l.Ref)
-                            listeLivrables.Add(l); // Si l'utilisateur entre un nombre, on associe ce nombre à la matière associée en XML et on ajoute cette matière à notre liste
+                        Console.WriteLine("Veuillez saisir la date de rendu.");
+                        l.dateRendu = Console.ReadLine();
+                        listeLivrables.Add(l); // Si l'utilisateur entre un nombre, on associe ce nombre à la matière associée en XML et on ajoute cette matière à notre liste
                     } // Il reste à gérer le cas où le nombre n'existe pas en bdd (Avec un while) pour lui demander de rééssayer sa saisie
                 }
             }
-
-
-
         }
-
-
     }
-}
 }
